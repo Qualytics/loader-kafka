e89193831fe537ca02bc67a5372c80571c4eb318
sophiekeller <skeller2022@gmail.com>
2020-10-16 11:52:39 -0400

    played with setup to get working correctly (confirmed by feeding into nifi)

--- setup.py
+++ setup.py
@@ -6,18 +6,11 @@ setup(
     version="0.1.0",
     description="Meltano loader for extracting data from a Kafka",
     author="Qualytics",
-    url="",
     classifiers=["Programming Language :: Python :: 3 :: Only"],
-    py_modules=["loader_kafka"],
-    install_requires=[
-        'jsonschema==2.6.0',
-                      'singer-python==2.1.4', 'requests>=2.4.2', 'python-dotenv','kafka-python']
-    ],
+    install_requires=['jsonschema==2.6.0',
+                      'singer-python==2.1.4', 'requests>=2.4.2', 'python-dotenv','kafka-python'],
     entry_points="""
     [console_scripts]
     loader-kafka=loader_kafka:main
-    """,
-    packages=["loader_kafka"],
-    package_data = {},
-    include_package_data=True,
+    """
 )
