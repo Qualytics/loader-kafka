5b0aabe861c7a16b8c0fadef4168f65bfba168f1
Eric Simmerman <ericsimmerman@gmail.com>
2020-12-09 11:53:40 -0500

    Fix depedency and package declaration

--- setup.py
+++ setup.py
@@ -6,11 +6,14 @@ setup(
     version="0.1.0",
     description="Meltano loader for publishing data to a Kafka topic",
     author="Qualytics",
+    url="https://github.com/Qualytics/loader-kafka",
     classifiers=["Programming Language :: Python :: 3 :: Only"],
-    install_requires=['jsonschema==2.6.0',
-                      'singer-python==2.1.4', 'kafka-python'],
+    py_modules=["loader_kafka"],
+    install_requires=['singer-python==2.1.4', 'kafka-python'],
     entry_points="""
     [console_scripts]
     loader-kafka=loader_kafka:main
-    """
+    """,
+    packages=["loader_kafka"],
+    include_package_data=True,
 )
